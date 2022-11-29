11387b44ba7c3c056d8c509282aa8964afc495a0
henriquelp321@gmail.com <henriquelp321@gmail.com>
2022-11-28 21:21:46 -0300

    [finishes #183906237] Feature terminada

--- esw/products.js
+++ esw/products.js
@@ -41,3 +41,15 @@ function listarCategoria(categorias){
 }
 let i = "Escolar"
 listarCategoria (i)
+
+function listarPreco(valorMininmo, valorMaximo){
+    for(let i = 0; i < produtos.length; i++){
+        if(produtos[i].valor >= valorMininmo && produtos[i].valor <= valorMaximo){
+            console.log(produtos[i])
+        }
+    }
+}
+
+let a = 2.0
+let b = 200.0
+listarPreco(a, b)
